ad7dc0981f0b000153600758af2bb85d113ef5d5
Abi Peterson <abipeterson07@gmail.com>
2017-02-01 23:24:47 +0000

    functions with parameters

--- script.js
+++ script.js
@@ -2,58 +2,64 @@ TinyTurtle.apply(window);
 
 penStyle = 'green'; 
 
-function square(){
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
-}
-
-function triangle(){
+function triangle(x){
     right(45);
-    forward(35);
+    forward(x);
     right(90);
-    forward(35);
+    forward(x);
     right(135);
-    forward(50);
+    forward(x);
 }
 
-function house(){
-    square();
-    triangle();
+function house(y){
+    square(y);
+    triangle(y);
 }
 
-function pentagon(){
+function pentagon(w){
     for(var i = 0; i <= 5; i++){
-    forward(40);
+    forward(w);
     right(72);
     }
 }
 
-function hexagon(){
+function hexagon(v){
     for(var i = 0; i <= 6; i++){
-    forward(20);
+    forward(v);
     right(60);
     }
 }
 
-function circle(){
-    for(var i = 0; i <= 360; i++){
-        forward(1);
-        right(90);
-        forward(1);
-        left(90);
+function square(width) {
+    forward(width);
+    right(90);
+    forward(width);
+    right(90);
+    forward(width);
+    right(90);
+    forward(width);
+    right(90);
+}
+
+function rotatedSquare(width, angle){
+    for(var i=0; i<(width/10); i ++){
+        square(width-(10*i));
+        left(angle);
+        //change by even and odd
     }
 }
+
+function shape(size, angle){
+    for(var i = 0; i < (360/angle); i++){
+        forward(size);
+        right(angle);
+}
+}
+//rotatedSquare(70, 50);
+shape(20, 67);
 // Type your function call below
-//square();
-//triangle();
-//house();
-//pentagon();
-//hexagon();
-circle();
+/*square(10);
+triangle(15);
+house(18);
+pentagon(20);
+hexagon(30);*/
